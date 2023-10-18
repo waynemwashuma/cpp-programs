9596c447cfbda8854e3dad72c32b98ceb1c1e249
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2023-10-18 05:31:58 -0700

    added while loop for persistence
    no

--- test/main.cpp
+++ test/main.cpp
@@ -2,11 +2,11 @@
 
 //using namespace std;
 int main(){
-    while(true){
+    //while(true){
         std::string str;
         std::cout << "enter something."<<std::endl;
         std::getline(std::cin,str);
         std::cout << "you entered:" << str <<std::endl;
-    }
+    //}
     return 0;
 }
